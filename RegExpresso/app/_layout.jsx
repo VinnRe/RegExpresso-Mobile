ae83dc2271e5efd87c6a9529ec1cf4fbc4641d5c
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-11-27 22:10:19 +0800

    Add simple routing to pages

--- RegExpresso/app/_layout.jsx
+++ RegExpresso/app/_layout.jsx
@@ -1,14 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <Stack>
+        <Stack.Screen name='index' options={{ 
+            headerShown: false
+        }} />
+        <Stack.Screen name="about" options={{ title: "About" }} />
+        <Stack.Screen name="login" options={{ title: "Login" }} />
+        <Stack.Screen name="signup" options={{ title: "Signup" }} />
+    </Stack>
   )
 }
 
 export default RootLayout
 
-const styles = StyleSheet.create({})
+// const styles = StyleSheet.create({
+//     contianer: {
+//         display: 'flex',
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     }
+// })
--- RegExpresso/app/index.jsx
+++ RegExpresso/app/index.jsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>RegExpresso</Text>
       <StatusBar style="auto" />
+      <Link href="/about" style={{ color: "blue" }}>Go to About</Link>
+      <Link href="/login" style={{ color: "blue" }}>Go to Login</Link>
+      <Link href="/signup" style={{ color: "blue" }}>Go to Signup</Link>
     </View>
   );
 }
