0d52142a0b18853ca7b07dc063c56899d8c344e0
Timothy Alimagno <davetimothyy@gmail.com>
2024-11-28 08:18:54 +0800

    added nativewind

--- RegExpresso/app/_layout.jsx
+++ RegExpresso/app/_layout.jsx
@@ -1,27 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import '../global.css'
 
 const RootLayout = () => {
   return (
     <Stack>
-        <Stack.Screen name='index' options={{ 
-            headerShown: false
-        }} />
-        <Stack.Screen name="about" options={{ title: "About" }} />
-        <Stack.Screen name="login" options={{ title: "Login" }} />
-        <Stack.Screen name="signup" options={{ title: "Signup" }} />
+      <Stack.Screen name='index' options={{
+        headerShown: false
+      }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+      <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen name="signup" options={{ title: "Signup" }} />
     </Stack>
   )
 }
 
 export default RootLayout
-
-// const styles = StyleSheet.create({
-//     contianer: {
-//         display: 'flex',
-//         flex: 1,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     }
-// })
--- RegExpresso/app/index.jsx
+++ RegExpresso/app/index.jsx
@@ -1,24 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <Text>RegExpresso</Text>
       <StatusBar style="auto" />
-      <Link href="/about" style={{ color: "blue" }}>Go to About</Link>
-      <Link href="/login" style={{ color: "blue" }}>Go to Login</Link>
-      <Link href="/signup" style={{ color: "blue" }}>Go to Signup</Link>
+      <Link href="/about">Go to About</Link>
+      <Link href="/login">Go to Login</Link>
+      <Link href="/signup">Go to Signup</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
