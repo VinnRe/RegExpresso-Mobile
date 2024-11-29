48ddf6ff1a3f307328dd713fa4bc55e6fbc20750
Timothy Alimagno <davetimothyy@gmail.com>
2024-11-29 23:37:06 +0800

    changes: used a variable for hiding header

--- RegExpresso/app/_layout.jsx
+++ RegExpresso/app/_layout.jsx
@@ -19,23 +19,15 @@ const RootLayout = () => {
 
   if (!fontsLoaded && !error) return null;
 
+  const screenOptions = {
+    headerShown: false,
+  }
+
   return (
     <Stack>
-      <Stack.Screen name='index' options={{
-        headerShown: false
-      }} />
-      <Stack.Screen
-        name='(auth)'
-        options={{
-          headerShown: false
-        }}
-      />
-      <Stack.Screen
-        name='(tabs)'
-        options={{
-          headerShown: false
-        }}
-      />
+      <Stack.Screen name="index" options={screenOptions} />
+      <Stack.Screen name="(auth)" options={screenOptions} />
+      <Stack.Screen name="(tabs)" options={screenOptions} />
     </Stack>
   )
 }
