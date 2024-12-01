f97c25ad8d98e574cf0428694cfb4894c0ed4e07
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-01 21:52:44 +0800

    Add logout logic and button

--- RegExpresso/app/(tabs)/profile.jsx
+++ RegExpresso/app/(tabs)/profile.jsx
@@ -3,8 +3,11 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react';
 import { router } from 'expo-router';
 import CustomButton from "../../components/CustomButton";
+import { useAuth, getUserData } from '../context/AuthContext';
 
 const Profile = () => {
+  const { handleLogout } = useAuth();
+
   return (
     <SafeAreaView className='bg-background-primary min-h-full px-10 justify-center items-center'>
       <CustomButton
@@ -15,6 +18,12 @@ const Profile = () => {
       <CustomButton
         title="Login"
         handlePress={() => { router.push('/login') }}
+        containerStyles="w-full mt-3"
+      />
+      
+      <CustomButton
+        title="Logout"
+        handlePress={() => { handleLogout(); router.push('/login'); }}
         containerStyles="w-full mt-3"
       />
     </SafeAreaView>
