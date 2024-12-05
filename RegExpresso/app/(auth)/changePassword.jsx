4a4e01cd98696ca3d9174eab8ad077f3534cd3fc
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-06 00:37:54 +0800

    Add change password logic

--- RegExpresso/app/(auth)/changePassword.jsx
+++ RegExpresso/app/(auth)/changePassword.jsx
@@ -5,12 +5,59 @@ import FormHeader from '../../components/FormHeader';
 import PasswordField from '../../components/PasswordField';
 import CustomButton from '../../components/CustomButton';
 import { router } from 'expo-router';
+import { useAuth } from '../../context/AuthContext';
+import { endpoints } from '../../constants/endpoints';
 
 const ChangePassword = () => {
+    const { token } = useAuth();
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
     const [form, setForm] = useState({
         newPassword: '',
         confirmNewPassword: '',
     });
+
+    const handleSubmit = async (e) => {
+        try {
+            if (form.newPassword !== form.confirmNewPassword) {
+                setErrorMessage('Passwords do not match.');
+                return;
+            }
+
+            if (form.newPassword.length < 8) {
+                setErrorMessage('Passwords must more than 8 characters long.');
+                return;
+            }
+            
+            setErrorMessage('');
+            console.log('Password change submitted:', form.newPassword);
+            
+            const response = await fetch(endpoints.updatePassword, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    password: form.newPassword
+                })
+            })
+            
+            if (response.ok) {
+                setSuccessMessage('Successfully changed your password!');
+            }
+            
+            if (!response.ok) {
+                setErrorMessage('Error changing password. Please try again later.');
+                throw new Error(`Error: ${response.status}`);
+            }
+            
+        } catch (error) {
+            setErrorMessage('Error changing password. Please try again later.');
+            console.error("Error: ", error)
+            throw error
+        }
+    };
 
     return (
         <SafeAreaView className="bg-background-primary h-full">
@@ -36,8 +83,27 @@ const ChangePassword = () => {
                         otherStyles="mt-3 flex-row"
                     />
 
+                    {errorMessage && (
+                        <Text className='text-text-error font-poppinsMedium text-lg mt-3'>
+                            {errorMessage}
+                        </Text>
+                    )}
+                    
+                    {successMessage && (
+                        <Text className='text-text text-center font-poppinsMedium text-lg mt-3'>
+                            {successMessage}
+                        </Text>
+                    )}
+
                     <CustomButton
-                        title="Cancel"
+                        title="Change Password"
+                        handlePress={handleSubmit}
+                        containerStyles="w-full mt-3"
+                        textStyles='text-2xl'
+                    />
+
+                    <CustomButton
+                        title="Back"
                         handlePress={() => {
                             router.back();
                         }}
