9768f924482223ef2c80dc416c8c2ffdd22bf563
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-06 23:53:48 +0800

    Fix error handler

--- RegExpresso/components/GraphComponent.jsx
+++ RegExpresso/components/GraphComponent.jsx
@@ -1,107 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { SvgXml } from 'react-native-svg';
-// import { Text } from 'react-native';
-// import { endpoints } from '../constants/endpoints';
-// import useRegexOptions from '../hooks/useRegexOptions';
-
-// const GraphComponent = ({ regEx, type }) => {
-//   const [svgContent, setSvgContent] = useState(null);
-//   const [errorMessage, setErrorMessage] = useState("");
-
-//   useEffect(() => {
-//     if (type === 'NFA') {
-//       const generateSvg = async (regEx) => {
-//         try {
-//           if (await regEx) {
-//             console.log("REGEX IN")
-//           }
-//           console.log(endpoints.svgNFA)
-//           const response = await fetch(endpoints.svgNFA, {
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ regEx }),
-//           });
-
-//           setErrorMessage("")
-
-//           if (!response.ok) {
-//             setErrorMessage("Error Visualizing Regular Expression.");
-//             return;
-//           }
-
-//           const data = await response.json();
-//           setSvgContent(data.svg);
-//         } catch (error) {
-//           setErrorMessage("Failed to fetch SVG, please try again later.")
-//           console.error('Failed to fetch SVG:', error);
-//         }
-//       };
-
-//       if (regEx) {
-//         generateSvg(regEx);
-//       }
-//     } else if (type === 'DFA') {
-//       const generateSvg = async (regEx) => {
-//         try {
-//           if (await regEx) {
-//             console.log("REGEX IN")
-//           }
-//           console.log(endpoints.svgDFA)
-//           const response = await fetch(endpoints.svgDFA, {
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ regEx }),
-//           });
-//           setErrorMessage("")
-
-//           if (!response.ok) {
-//             setErrorMessage("Error Visualizing Regular Expression.");
-//             return;
-//           }
-
-//           const data = await response.json();
-//           setSvgContent(data.svg);
-//         } catch (error) {
-//           setErrorMessage("Failed to fetch SVG, please try again later.")
-//           console.error('Failed to fetch SVG:', error);
-//         }
-//       };
-
-//       if (regEx) {
-//         generateSvg(regEx);
-//       }
-//     } else {
-//       return
-//     }
-
-//   }, [regEx, type]);
-
-//   if (!svgContent) {
-//     return null;
-//   }
-
-//   return (
-//     <>
-//       {errorMessage ? (
-//         <>  
-//           <Text className='text-text-error font-poppinsMedium text-lg mt-3'>
-//             {errorMessage}
-//           </Text>
-//         </>
-//       ) : (
-//         <SvgXml xml={svgContent} width="100%" height="100%" style={{ marginLeft: -40, zIndex: -1 }} />
-//       )}
-//     </>
-
-//   );
-// };
-
-// export default GraphComponent;
-
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { SvgXml } from 'react-native-svg';
@@ -114,7 +10,10 @@ const GraphComponent = ({ regEx, type }) => {
   useEffect(() => {
     const generateSvg = async (url, regEx) => {
       try {
-        if (!regEx) return;
+        if (!regEx) {
+          setErrorMessage("");
+          return;
+        };
 
         const response = await fetch(url, {
           method: 'POST',
@@ -123,15 +22,15 @@ const GraphComponent = ({ regEx, type }) => {
         });
 
         if (!response.ok) {
-          throw new Error('Error visualizing Regular Expression.');
+          setErrorMessage("Error visualizing regular expression. Please try again.");
+        } else {
+          setErrorMessage("")
         }
 
         const data = await response.json();
         setSvgContent(data.svg);
-        setErrorMessage("");
       } catch (error) {
         setErrorMessage("Failed to fetch SVG, please try again later.");
-        console.error('Failed to fetch SVG:', error);
       }
     };
 
