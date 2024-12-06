afb4707db0396c55c6cfab681b8febe998ed2bde
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-07 00:12:47 +0800

    Update about text

--- RegExpresso/app/about.jsx
+++ RegExpresso/app/about.jsx
@@ -21,9 +21,9 @@ const About = () => {
         </TouchableOpacity>
         <Text className="text-DEFAULT text-5xl font-bold mb-10">About Us</Text>
         <Text className="text-DEFAULT text-2xl font-poppinsMedium text-justify mb-10">
-          RegExpresso is a web application that converts Regular Expressions to Finite State Machines.
-          TripleThreat created this web application for users to visualize their regular expressions in a simple
-          and easy to use interface.
+          RegExpresso is a mobile application that converts Regular Expressions to visualize their Finite State Machines.
+          TripleThreat created this mobile application for users to visualize their regular expressions in a simple
+          and easy to use interface. Have fun using RegExpresso, and start visualizing!
         </Text>
         <Text className="text-DEFAULT text-2xl font-poppinsMedium">Copyright © 2024 TripleThreat</Text>
       </ImageBackground>
