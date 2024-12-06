c377a07885fdf2a3295e9646bbe2cb13cc190357
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-06 10:29:56 +0800

    Update Error Handler

--- RegExpresso/components/GraphComponent.jsx
+++ RegExpresso/components/GraphComponent.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { SvgXml } from 'react-native-svg';
+import { Text } from 'react-native';
 import { endpoints } from '../constants/endpoints';
 import useRegexOptions from '../hooks/useRegexOptions';
 
 const GraphComponent = ({ regEx, type }) => {
   const [svgContent, setSvgContent] = useState(null);
-  const { errorMessageSave, setErrorMessageSave } = useRegexOptions();
-
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     if (type === 'NFA') {
@@ -23,9 +23,18 @@ const GraphComponent = ({ regEx, type }) => {
             },
             body: JSON.stringify({ regEx }),
           });
+
+          setErrorMessage("")
+
+          if (!response.ok) {
+            setErrorMessage("Error Visualizing Regular Expression.");
+            return;
+          }
+          
           const data = await response.json();
           setSvgContent(data.svg);
         } catch (error) {
+          setErrorMessage("Failed to fetch SVG, please try again later.")
           console.error('Failed to fetch SVG:', error);
         }
       };
@@ -47,9 +56,17 @@ const GraphComponent = ({ regEx, type }) => {
             },
             body: JSON.stringify({ regEx }),
           });
+          setErrorMessage("")
+
+          if (!response.ok) {
+            setErrorMessage("Error Visualizing Regular Expression.");
+            return;
+          }
+
           const data = await response.json();
           setSvgContent(data.svg);
         } catch (error) {
+          setErrorMessage("Failed to fetch SVG, please try again later.")
           console.error('Failed to fetch SVG:', error);
         }
       };
@@ -68,7 +85,18 @@ const GraphComponent = ({ regEx, type }) => {
   }
 
   return (
-    <SvgXml xml={svgContent} width="100%" height="100%" style={{ marginLeft: -40, zIndex: -1 }} />
+    <>
+      {errorMessage ? (
+        <>  
+          <Text className='text-text-error font-poppinsMedium text-lg mt-3'>
+            {errorMessage}
+          </Text>
+        </>
+      ) : (
+        <SvgXml xml={svgContent} width="100%" height="100%" style={{ marginLeft: -40, zIndex: -1 }} />
+      )}
+    </>
+    
   );
 };
 
